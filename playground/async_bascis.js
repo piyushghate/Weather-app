f297d7c621c6ddd6508a1e4ce8682cd3491bcc3a
piyushghate <piyushghate17@gmail.com>
2018-09-27 11:42:51 +0530

    This is async-basics commit

---
+++ playground/async_bascis.js
@@ -0,0 +1,7 @@
+console.log('Starting app..');
+
+setTimeout(() => {
+    console.log('Inside of CallBack!');
+}, 2000);                               //2000 = 2 sec
+
+console.log('Finishing up...');
