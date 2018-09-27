5ad773bce3b90abd5773a307b959287b604877b7
piyushghate <piyushghate17@gmail.com>
2018-09-27 12:33:24 +0530

    async-basics modified to show two examples

--- playground/async_bascis.js
+++ playground/async_bascis.js
@@ -1,7 +1,24 @@
+/*This code will help to illustrate the basics of 
+1. Call Stack
+2. Node API's
+3. Callback Queue
+4. Event loop*/
+
 console.log('Starting app..');
 
 setTimeout(() => {
-    console.log('Inside of CallBack!');
+    console.log('Inside of Timeout with 2 sec!');
 }, 2000);                               //2000 = 2 sec
 
+setTimeout(() => {
+    console.log('Inside 2nd Timeout with 0 sec');
+}, 0);                                 //0 = 0sec
+
 console.log('Finishing up...');
+
+
+/*OutPut:
+Starting app..
+Finishing up...
+Inside 2nd Timeout with 0 sec
+Inside of Timeout with 2 sec!*/
