d12a50d9b3c300a4cd89dcb17a325113bea5813d
piyushghate <piyushghate17@gmail.com>
2018-09-28 10:55:08 +0530

    User input address which results in Logitute and Latitude

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+console.log('Starting WebAPP!!');
+
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for!',
+        string: true,
+    },
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+var encoded = encodeURIComponent(argv.address);
+
+console.log(argv);
+console.log('argv.a: ', encoded );
 
 request({
-    url:'http://www.mapquestapi.com/geocoding/v1/address?key=a4D0zduv0kbUOIue2amAwAumYTiGFONF&location=nasik',
+    url:`http://www.mapquestapi.com/geocoding/v1/address?key=a4D0zduv0kbUOIue2amAwAumYTiGFONF&location=${encoded}`,
     json:true,
 },(error, response, body) => {
     //console.log(JSON.stringify(body, undefined, 2));
