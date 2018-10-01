1b46ec14c8ed83ee8bbe36726a98822ed695de46
piyushghate <piyushghate17@gmail.com>
2018-10-01 23:40:07 +0530

    Weather App with full functionality

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 console.log('Starting WebAPP!!');
 
-const request = require('request');
-const yargs = require('yargs');
+
+const yargs = require('yargs');                     // import yargs to simplify user input in terminal
+
+const geocode = require('./geocode/geocode.js');
+const weather = require('./weather/weather.js');
 
 const argv = yargs
 .options({
@@ -16,17 +19,24 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-var encoded = encodeURIComponent(argv.address);
+//console.log(argv);
+//console.log('argv.a: ', encoded );
 
-console.log(argv);
-console.log('argv.a: ', encoded );
+geocode.geocodeAddress(argv.address, (errorMessage, results)=>{
+    if(errorMessage){
+        console.log(errorMessage);
+    }else {
+        console.log('City: ', results.City);
+        console.log('State: ', results.State);
+        console.log('Country Code: ', results.Country_Code);
+        //console.log(results, undefined, 2);
+        weather.getTemp(results.Latitude, results.Longitude, (errorMessage, results) =>{
+            if (errorMessage){
+                console.log(errorMessage);
+            }else{
+                console.log(`Current temperature is ${results.CelsiusTemperature} degree celsius, but apprently it feels like ${results.celsiusApparent} degree celsius!!`);
+            }
+        });
+    }
 
-request({
-    url:`http://www.mapquestapi.com/geocoding/v1/address?key=a4D0zduv0kbUOIue2amAwAumYTiGFONF&location=${encoded}`,
-    json:true,
-},(error, response, body) => {
-    //console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Address: ${body.results[0].providedLocation.location}`);
-    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
 });
